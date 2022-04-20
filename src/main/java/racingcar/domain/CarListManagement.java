3ec9b349008573f7dbd88f6487eb2066a3654477
Domaksaramu <jewon945@gmail.com>
2022-04-20 22:46:21 +0900

    feat: CarListObject 생성 및 테스트 작성

--- src/main/java/racingcar/domain/CarListManagement.java
+++ src/main/java/racingcar/domain/CarListManagement.java
@@ -1,2 +1,88 @@
-package racingcar.domain;public class CarListManagement {
+package racingcar.domain;
+
+import java.util.*;
+
+
+public class CarListManagement {
+    private List<CarObject> carsList;
+    /////////////private Set<String> setForDuplicateNames;
+    private Integer carsNumber = 0;
+
+    public CarListManagement(List<String> carNames){
+        this.carsNumber = carNames.size();
+        carsList = initCarList(carNames);
+    }
+    public CarListManagement(){
+        this.carsList = new ArrayList<>();
+    }
+
+    private List<CarObject> initCarList(List<String> carNames) {
+        List<CarObject> resultList = new ArrayList<>();
+        for(String carName : carNames){
+            resultList.add(new CarObject(carName));
+        }
+        return resultList;
+    }
+
+    /**
+     * 전진횟수를 내림차순으로 정렬 (comparable.compareTo가 내림차순 기준)
+     * @return
+     */
+    public List<CarObject> sortCarsList(){
+        Collections.sort(this.carsList);
+        return this.carsList;
+    }
+
+    public Integer getCarsNumber() {
+        return carsNumber;
+    }
+
+    public List<CarObject> getCarsList() {
+        return carsList;
+    }
+
+    public void setCarsList(List<CarObject> carsList) {
+        this.carsList = carsList;
+        this.carsNumber = carsList.size();
+    }
+
+    public void setCarsNumber(Integer carsNumber) {
+        this.carsNumber = carsNumber;
+    }
+    public void addCar(String carName){
+        CarObject car = new CarObject(carName);
+        this.carsList.add(car);
+    }
+    public void addCar(CarObject car){
+        this.carsList.add(car);
+    }
+
+    /**
+     * 자동차 리스트에서 Max값을 추출하여
+     * 해당 값과 동일한 값의 자동차 리스트를 추출
+     * (내림차순으로 정렬하므로, index가 0인 값이 Max)
+     * @return
+     */
+    public List<CarObject> getMaxCarObjects(){
+        List<CarObject> resultList = new ArrayList<>();
+        sortCarsList();
+        CarObject maxCar = new CarObject(this.carsList.get(0));
+        for (CarObject car: this.carsList) {
+            if(maxCar.getForwardCount() == car.getForwardCount()){
+                resultList.add(new CarObject(car));
+            }
+        }
+        return resultList;
+    }
+    public void verifyDuplicateNames(){
+
+        return;
+    }
+    @Override
+    public String toString() {
+        return "{" +
+                "carsList=" + carsList +
+                ", carsNumber=" + carsNumber +
+                '}';
+    }
 }
--- src/test/java/racingcar/domain/CarListManagemnetTest.java
+++ src/test/java/racingcar/domain/CarListManagemnetTest.java
@@ -1,2 +1,57 @@
-package racingcar.domain;public class CarListManagemnetTest {
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CarListManagemnetTest {
+    @Test
+    public void generateCarsListTest(){
+        List<String> carsNamesList = new ArrayList<>();
+        carsNamesList.add("TEST1");
+        carsNamesList.add("TEST2");
+        carsNamesList.add("TEST3");
+        CarListManagement carListManagement = new CarListManagement(carsNamesList);
+        assertThat(carListManagement.toString()).isEqualTo("{carsList=[{carName='TEST1', forwardCount=0, currentRoundNumber=-1}, {carName='TEST2', forwardCount=0, currentRoundNumber=-1}, {carName='TEST3', forwardCount=0, currentRoundNumber=-1}], carsNumber=3}");
+    }
+
+    @Test
+    public void carListSortTest(){
+        CarListManagement carListManagement = new CarListManagement();
+        List<CarObject> carList = new ArrayList<>();
+        CarObject car1 = new CarObject("TEST1");
+        car1.setForwardCount(1);
+        carList.add(car1);
+        CarObject car2 = new CarObject("TEST2");
+        car2.setForwardCount(2);
+        carList.add(car2);
+        CarObject car3 = new CarObject("TEST3");
+        car3.setForwardCount(3);
+        carList.add(car3);
+        carListManagement.setCarsList(carList);
+        carListManagement.sortCarsList();
+        assertThat(carListManagement.toString()).isEqualTo("{carsList=[{carName='TEST3', forwardCount=3, currentRoundNumber=-1}, {carName='TEST2', forwardCount=2, currentRoundNumber=-1}, {carName='TEST1', forwardCount=1, currentRoundNumber=-1}], carsNumber=3}");
+    }
+    @Test
+    public void getMaxCarObjectsTest(){
+        CarListManagement carListManagement = new CarListManagement();
+        List<CarObject> carList = new ArrayList<>();
+        CarObject car1 = new CarObject("TEST1");
+        car1.setForwardCount(1);
+        carList.add(car1);
+        CarObject car2 = new CarObject("TEST2");
+        car2.setForwardCount(2);
+        carList.add(car2);
+        CarObject car3 = new CarObject("TEST3");
+        car3.setForwardCount(3);
+        carList.add(car3);
+        CarObject car4 = new CarObject("TEST4");
+        car4.setForwardCount(3);
+        carList.add(car4);
+        carListManagement.setCarsList(carList);
+        assertThat(carListManagement.getMaxCarObjects().toString()).isEqualTo("[{carName='TEST3', forwardCount=3, currentRoundNumber=-1}, {carName='TEST4', forwardCount=3, currentRoundNumber=-1}]");
+    }
 }
