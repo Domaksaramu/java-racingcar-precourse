f2841003d9e87ec09d223ddcb02fd04e63d47386
Domaksaramu <jewon945@gmail.com>
2022-04-21 09:43:48 +0900

    feat: RandomGenerateService 생성 및 테스트 추가

--- src/main/java/racingcar/service/RandomGenerateService.java
+++ src/main/java/racingcar/service/RandomGenerateService.java
@@ -1,2 +1,15 @@
-package racingcar.domain;public class RandomGenerateService {
+package racingcar.service;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomGenerateService {
+    private static final Integer MAX_NUMBER_RANGE = 9+1;
+    private static final Integer MIN_NUMBER_RANGE = 0;
+
+    public static Integer generateRandomInteger(){
+        Integer result = -1;
+        result = ThreadLocalRandom.current().nextInt(MIN_NUMBER_RANGE,MAX_NUMBER_RANGE) ;
+        return result;
+    }
 }
